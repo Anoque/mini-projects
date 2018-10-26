2a9d5e54c0f8948454f541c30e38d223891ff696
Unknown <anoque@mail.ru>
2018-10-27 00:49:31 +0700

    Update pads

---
+++ Pads/README.md
@@ -0,0 +1,7 @@
+# Pads
+
+### Mini settings
+
+> Key 1 - boring mode
+
+> Key 2 - crazy mode
--- Pads/sketch.js
+++ Pads/sketch.js
@@ -5,27 +5,43 @@ var	pads = [],
 	y = 10,
 	xp = 0,
 	yp = 0,
-	bg = [255, 255, 255];
+	x1 = 0, 
+	y1 = 0,
+	bg = [255, 255, 255],
+	type = 1;
 
 function setup() {
 	createCanvas(document.body.clientWidth, document.body.clientHeight);
 	frameRate(100);
+
 	xp = document.body.clientWidth / x;
 	yp = document.body.clientHeight / y;
 
 	for (var i = 0; i < x; i++) {
 		pads.push([]);
-		for (var j = 0; j < y; j++) {
+
+		for (var j = 0; j < y; j++)
 			pads[i].push([i * (document.body.clientWidth / x), j * (document.body.clientHeight / y), bg, 0]);
-		}
 	}
 }
 
 function draw() {
 	background(bg[0], bg[1], bg[2]);
+	setDirection();
+
 	for (var i = 0; i < x; i++) {
 		for (var j = 0; j < y; j++) {
-			if ((pads[i][j][0] <= mouseX) && ((pads[i][j][0] + xp) >= mouseX) && (pads[i][j][1] <= mouseY) && ((pads[i][j][1] + yp) >= mouseY)) {
+			var show = false;
+
+			if (type == 1) {
+				if ((pads[i][j][0] <= mouseX) && ((pads[i][j][0] + xp) >= mouseX) && (pads[i][j][1] <= mouseY) && ((pads[i][j][1] + yp) >= mouseY))
+					show = true;
+			} else if (type == 2) {
+				if (i == x1 && j == y1)
+					show = true;
+			}
+
+			if (show) {
 				if (pads[i][j][3] < 1) {
 					pads[i][j][2][0] = random(0, 255);
 					pads[i][j][2][1] = random(0, 255);
@@ -39,5 +55,39 @@ function draw() {
 			fill(pads[i][j][2][0] * pads[i][j][3], pads[i][j][2][1] * pads[i][j][3], pads[i][j][2][2] * pads[i][j][3]);
 			rect(pads[i][j][0], pads[i][j][1], pads[i][j][0] + xp, pads[i][j][1] + yp);
 		}
+	}
+}
+
+function setDirection() {
+	var vec = Math.floor(random(0, 4));
+
+	if (vec == 0) {
+		if (x1 < x - 1)
+			x1 += 1;
+		else
+			setDirection();
+	} else if (vec == 1) {
+		if (y1 < y - 1)
+			y1 += 1;
+		else
+			setDirection();
+	} else if (vec == 2) {
+		if (x1 > 0)
+			x1 -= 1;
+		else
+			setDirection();
+	} else if (vec == 3) {
+		if (y1 > 0)
+			y1 -= 1;
+		else
+			setDirection();
+	}
+}
+
+function keyPressed() {
+	if (key == "1") {
+		type = 1;
+	} else if (key == "2") {
+		type = 2;
 	}
 }
