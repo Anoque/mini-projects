08b531babd6ff9876c15101c9bb0fe046c56404b
Unknown <anoque@mail.ru>
2018-10-27 00:17:09 +0700

    Delete comments

--- Pads/sketch.js
+++ Pads/sketch.js
@@ -16,7 +16,6 @@ function setup() {
 	for (var i = 0; i < x; i++) {
 		pads.push([]);
 		for (var j = 0; j < y; j++) {
-			// console.log((i * (document.body.clientWidth / x)) + " " + (j * (document.body.clientHeight / y)));
 			pads[i].push([i * (document.body.clientWidth / x), j * (document.body.clientHeight / y), bg, 0]);
 		}
 	}
@@ -26,7 +25,6 @@ function draw() {
 	background(bg[0], bg[1], bg[2]);
 	for (var i = 0; i < x; i++) {
 		for (var j = 0; j < y; j++) {
-			// if (mouseIsPressed) {
 			if ((pads[i][j][0] <= mouseX) && ((pads[i][j][0] + xp) >= mouseX) && (pads[i][j][1] <= mouseY) && ((pads[i][j][1] + yp) >= mouseY)) {
 				if (pads[i][j][3] < 1) {
 					pads[i][j][2][0] = random(0, 255);
@@ -37,7 +35,7 @@ function draw() {
 			} else if (pads[i][j][3] > 0) {
 				pads[i][j][3] -= pads[i][j][3] * .1;
 			}
-			// }
+
 			fill(pads[i][j][2][0] * pads[i][j][3], pads[i][j][2][1] * pads[i][j][3], pads[i][j][2][2] * pads[i][j][3]);
 			rect(pads[i][j][0], pads[i][j][1], pads[i][j][0] + xp, pads[i][j][1] + yp);
 		}
